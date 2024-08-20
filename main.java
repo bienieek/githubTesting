9465ad426bc2dd9df3338d91f4ffea92a8ec1c67
bienieek <83711290+bienieek@users.noreply.github.com>
2024-08-21 01:43:56 +0200

    poprawka_2

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("INIT ONE");
         System.out.println("Poprawka 1");
+        System.out.println("Poprakwa 2");
     }
 }
